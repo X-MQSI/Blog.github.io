022ba3c25c70998cd7ad7ffda87e74fc05a517f0
HKEMS-STMO_MQSI <118874898+X-MQSI@users.noreply.github.com>
2022-12-10 10:25:44 +0800

    Update shici.js

--- js/shici.js
+++ js/shici.js
@@ -1,5 +1,5 @@
   var xhr = new XMLHttpRequest();
-  xhr.open('get', 'https://v1.jinrishici.com/all.txt');
+  xhr.open('get', 'https://v1.jinrishici.com/tianqi.txt');
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
       var gushici = document.getElementById('gushici');
